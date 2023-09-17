7177861bbc9077331c02bdf14ad485be37471478
Aed P <54215788+foxy4096@users.noreply.github.com>
2023-09-17 22:02:11 +0530

    God help me

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Lord have mercy on us! \n"); // DON'T DO THIS! Never remove this line!
+
+    let my_awesome_variable: u32 = 69;
+    println!("{my_awesome_variable}");
+    println!("Do you want to change the my_awesome_variable?");
+    println!("Y/n");
+    let mut choice = String::new();
+    println!("This is without &: {my_awesome_variable}");
+    println!("{}", &my_awesome_variable);
+
+    std::io::stdin().read_line(&mut choice).expect("Oh Fuck!");
 }
